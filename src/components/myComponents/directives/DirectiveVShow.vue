97152246fc46746fa56eb118e516988549c68131
VeronikaKoma <116543383+VeronikaKoma@users.noreply.github.com>
2023-04-06 16:24:15 +0200

    add btn

---
+++ src/components/myComponents/directives/DirectiveVShow.vue
@@ -0,0 +1,21 @@
+
+<script>
+export default {
+
+}
+</script>
+
+
+<template>
+    <div>
+        <h3>---- v-show directive ----</h3>
+        <button v-on:click="changeState"></button>
+        <span v-show="isShowing">Sundance...</span>
+        <button>
+    </div>
+</template>
+
+
+<style>
+
+</style>
